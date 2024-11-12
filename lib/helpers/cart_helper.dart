1eee1e2031027fcf8ab2d2514c2b64f1ef8b9931
Victor Eduardo Garcia Sardaneta <regulusleoc@gmail.com>
2024-11-11 18:49:45 -0600

    Hot Fix - Stock limit

--- lib/helpers/cart_helper.dart
+++ lib/helpers/cart_helper.dart
@@ -9,27 +9,40 @@ class CartHelper {
     final prefs = await SharedPreferences.getInstance();
     final cart = await getCart();
 
-    final existingProduct = cart.firstWhere(
-      (item) => item['id'] == product.id,
-      orElse: () => {},
-    );
+    final existingProductIndex = cart.indexWhere((item) => item['id'] == product.id);
+    Map<String, dynamic> existingProduct = existingProductIndex != -1
+        ? cart[existingProductIndex]
+        : {};
 
     if (existingProduct.isNotEmpty) {
-      existingProduct['quantity'] += quantity;
-      existingProduct['total'] = existingProduct['quantity'] * product.price;
+      // Update existing product quantity within stock limits
+      int currentQuantity = existingProduct['quantity'] ?? 0;
+      int newQuantity = currentQuantity + quantity;
+
+      if (newQuantity > product.stock) {
+        throw Exception("Not enough stock. You can only add up to ${product.stock - currentQuantity} more.");
+      }
+
+      cart[existingProductIndex]['quantity'] = newQuantity;
+      cart[existingProductIndex]['total'] = newQuantity * product.price;
     } else {
-      if (cart.length < 7) {
-        cart.add({
-          'id': product.id,
-          'name': product.title,
-          'quantity': quantity,
-          'price': product.price,
-          'total': quantity * product.price,
-          'thumbnail': product.thumbnail,
-        });
-      } else {
+      // Add new product if within cart limit
+      if (cart.length >= 7) {
         throw Exception("You can only add up to 7 unique products.");
       }
+
+      if (quantity > product.stock) {
+        throw Exception("Not enough stock. Maximum available: ${product.stock}");
+      }
+
+      cart.add({
+        'id': product.id,
+        'name': product.title,
+        'quantity': quantity,
+        'price': product.price,
+        'total': quantity * product.price,
+        'thumbnail': product.thumbnail,
+      });
     }
 
     await prefs.setString(cartKey, jsonEncode(cart));
@@ -39,7 +52,11 @@ class CartHelper {
     final prefs = await SharedPreferences.getInstance();
     final cartString = prefs.getString(cartKey);
     if (cartString != null) {
-      return List<Map<String, dynamic>>.from(jsonDecode(cartString));
+      try {
+        return List<Map<String, dynamic>>.from(jsonDecode(cartString));
+      } catch (e) {
+        print("Error decoding cart JSON: $e");
+      }
     }
     return [];
   }
